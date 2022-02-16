9bf1cca10fa6198a54bdfb0289dc79ab0c52084b
EDUARDO RACY <RACYEDU000@auburn.wednet.edu>
2022-02-16 13:46:14 -0800

    snackMethods

--- Unit 5/Unit 5/src/Snack.java
+++ Unit 5/Unit 5/src/Snack.java
@@ -1,14 +1,16 @@
 public class Snack {
+
     private String name;
     private int calories;
+
+    public Snack() {
+        name = "Air";
+        calories = 0;
+    }
 
     public Snack(String name, int calories) {
         this.name = name;
         this.calories = calories;
-    }
-    public Snack() {
-        name = "Air";
-        calories = 0;
     }
 
     public void setName(String name) {
@@ -26,14 +28,25 @@ public class Snack {
     public int getCalories() {
         return calories;
     }
+
+    public boolean isSameName(Snack s2) {
+        return this.name.equals(s2.name);
+
+    }
+
     public String toString() {
-        return "The snack " + name + " has " + calories + ".";
+        String result = "The snack " + name;
+        result.concat(" has " + calories + ".");
+
+        // return result;
+        // return String.format("The snack %s has %s calories.", name, calories);
+        return new StringBuffer("The snack ").append(name).append(" has ").append(calories).toString();
     }
 
     public static void main(String[] args) {
         UT.clearScreen();
         Snack s1 = new Snack("Kit-Kat", 300);
-        Snack s2 = new Snack();
+        Snack s2 = new Snack("Snickers", 200);
         s1.setName("Snickers");
         System.out.println(s1.getName());
         s1.setCalories(200);
@@ -41,5 +54,9 @@ public class Snack {
         System.out.println(s1);
         System.out.println();
         System.out.println(s2);
+        System.out.println();
+        System.out.println(s1.getName().equals(s2.getName()));
+        System.out.println(s1.getCalories() == s2.getCalories());
+        System.out.println(s1.isSameName(s2));
     }
 }
