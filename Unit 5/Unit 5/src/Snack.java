7e61d7213c81cbc713accdc0f9b5deb4fdf5c895
EDUARDO RACY <RACYEDU000@auburn.wednet.edu>
2022-02-17 13:46:32 -0800

    snackComments

--- Unit 5/Unit 5/src/Snack.java
+++ Unit 5/Unit 5/src/Snack.java
@@ -1,45 +1,67 @@
+/*
+    Programmer: Eduardo Racy
+    Date: 02/17/2022
+    Purpose: to represent a snack
+*/
+
+
 public class Snack {
 
     private String name;
     private int calories;
 
+    //Deafult constructor
     public Snack() {
         name = "Air";
         calories = 0;
     }
-
+    //Overloaded constructor
     public Snack(String name, int calories) {
         this.name = name;
         this.calories = calories;
     }
-
+    //Mutator
     public void setName(String name) {
         this.name = name;
     }
-
+    //Accessor
     public String getName() {
         return name;
     }
-
+    //Mutator
     public void setCalories(int calories) {
         this.calories = calories;
     }
-
+    //Accessor
     public int getCalories() {
         return calories;
     }
-
+    //Method
     public boolean isSameName(Snack s2) {
         return this.name.equals(s2.name);
-
     }
-
+    /**
+     * Method ot set value of calories
+     * @param s2 - value to update number of calories
+     * @return
+     */
+    //Method
+    public boolean isSameCalories(Snack s2) {
+        return this.calories == s2.calories;
+    }
+    //toString
     public String toString() {
-        String result = "The snack " + name;
+        /* String result = "The snack " + name;
         result.concat(" has " + calories + ".");
+        return result;
+        */
 
-        // return result;
         // return String.format("The snack %s has %s calories.", name, calories);
+
+        // MessageFormat.format("The snack {0} has {1} calories.", name, calories);
+
+        // return "The snack %s has %s calories".formatter
+
         return new StringBuffer("The snack ").append(name).append(" has ").append(calories).toString();
     }
 
@@ -58,5 +80,11 @@ public class Snack {
         System.out.println(s1.getName().equals(s2.getName()));
         System.out.println(s1.getCalories() == s2.getCalories());
         System.out.println(s1.isSameName(s2));
+
+        if (s1.isSameCalories(s2)) {
+            System.out.println("Both snacks have the same amount of calories");
+        } else {
+            System.out.println("Both snacks do not have the same amount of calories");
+        }
     }
 }
