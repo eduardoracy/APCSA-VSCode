27fcb101f1e982cd22664e2bf7b8c7e60326014f
EDUARDO RACY <RACYEDU000@auburn.wednet.edu>
2022-03-01 12:56:19 -0800

    Array Intro

---
+++ Unit 5/Unit 5/src/ArrayPlay.java
@@ -0,0 +1,23 @@
+
+public class ArrayPlay {
+    public static void main(String[] args) {
+        UT.clearScreen(20);
+        int[] scores = new int[8];
+        for (int i = 0; i < scores.length; i++) {
+            scores[i] = 0;
+            System.out.print(scores[i] + " ");
+        }
+        scores[0] = 60;
+        scores[7] = 92;
+        System.out.println();
+        for (int i = 0; i < scores.length; i++) {
+            System.out.print(scores[i] + " ");
+        }
+        scores[1] = (int) (Math.random() * 100);
+        System.out.println();
+        for (int i = 0; i < scores.length; i++) {
+            System.out.print(scores[i] + " ");
+        }
+        System.out.println("\n"); 
+    }
+}
