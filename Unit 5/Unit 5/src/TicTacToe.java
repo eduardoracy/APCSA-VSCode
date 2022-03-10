c861e0c606bd3f550513ae372009697206ddb95c
EDUARDO RACY <RACYEDU000@auburn.wednet.edu>
2022-03-10 13:45:32 -0800

    TicTacToe yahtzee imports

--- Unit 5/Unit 5/src/TicTacToe.java
+++ Unit 5/Unit 5/src/TicTacToe.java
@@ -1,47 +1,104 @@
+import java.util.Scanner;
+
 public class TicTacToe {
     int[][] board = new int[3][3];
+    int turnCounter = 0;
+    boolean startScreen = false;
+    Scanner console = new Scanner(System.in);
+    String SS;
+    String p1, p2;
 
     public static void main(String[] args) {
-        UT.clearScreen();
         new TicTacToe().pBoard();
     }
 
+    public void startScreen() {
+		UT.clearScreen();
+		if (!startScreen) {
+			UT.textCenter("Press ENTER to start...", ' ', 25);// classic start screen
+			SS = console.nextLine();
+
+			UT.clearScreen();
+			startScreen = true;
+		}
+		UT.textCenter(" Welcome to Tic-Tac-Toe!! ", '═', 25);
+		UT.textCenter(" Designed by Eduardo Racy ", '─', 25);
+		System.out.println("1. Start Game");
+		System.out.println("2. Instructions");
+		SS = console.next();
+		UT.clearScreen();
+		if (SS.equals("1")) {
+			nameSelect();
+			firstPlayer();
+		} else if (SS.equals("2")) {
+			//instructions();
+			startScreen();
+		} else {
+            UT.clearScreen();
+            UT.textCenter(" Welcome to Tic-Tac-Toe!! ", '═', 25);
+		    UT.textCenter(" Designed by Eduardo Racy ", '─', 25);
+            System.out.println("Invalid Selection");
+            System.out.println("Press ENTER to try again...");
+            SS = console.nextLine();
+            startScreen();
+        }
+	}
+
+    public void firstPlayer() {
+
+    }
+
+    public void nameSelect() {
+		UT.textCenter(" New game ", '═', 25);
+		UT.textCenter(" Name Selection ", '─', 25);
+		System.out.println();
+		UT.textCenter("Please enter a name with 9 characters or less.", ' ', 25);
+		System.out.println("\nPlayer 1 enter your name...");
+		SS = console.next();
+		p1 = SS.substring(0, 1).toUpperCase() + SS.substring(1).toLowerCase();
+		System.out.println("\nPlayer 2 enter your name...");
+		SS = console.next();
+		p2 = SS.substring(0, 1).toUpperCase() + SS.substring(1).toLowerCase();
+		System.out.println();
+		UT.textCenter("Press ENTER to continue...", ' ', 25);
+		SS = console.nextLine();
+		UT.clearScreen();
+	}
+
     public void pBoard() {
-        for (int i = 1; i <= 11; i++){
-        if (i % 4 == 0) {
-            for (int j = 0; j < 3; j++) {
-                System.out.print("\u2501\u2501\u2501");
-                if (j < 3) {
-                    System.out.print("\u254B");
+        for (int i = 1; i <= 5; i++){
+        if (i % 2 == 0) { // 2 Middle cross rows
+            for (int col = 0; col < 3; col++) {
+                System.out.print("───");
+                if (col < 2) {
+                    System.out.print("┼");
                 }
             }
-        } else if (i % 2 == 0) {
+        } else { //Rows with user values
             char symb;
-            for (int j = 0; j < 3; j++) {
-                switch (board[i][j]) {
+            int count = 0;
+            int row = i;
+            while (row % 2 > 1) {
+            row /= 2;
+            count++;
+            }
+            for (int col = 0; col < 3; col++) {
+                switch (board[count][col]) {
                     case 1:
-                    symb = '\u2573';
+                    symb = 'X';
                     break;
                     case 2:
-                    symb = '\u2573';
+                    symb = 'O';
                     break;
                     default:
                     symb = ' ';
                     break;
-                }
+                } 
                 System.out.print(String.format(" %s ", symb));
-                if (j < 3) {
-                    System.out.print("\u2503");
+                if (col < 2) {
+                    System.out.print("│");
                 }
             }
-        } else {
-                for (int j = 0; j < 3; j++) {
-                    System.out.print("   ");
-                    if (j < 3) {
-                        System.out.print("\u2503");
-                    }
-                }
-                System.out.println();
         }
         System.out.println();
     }
