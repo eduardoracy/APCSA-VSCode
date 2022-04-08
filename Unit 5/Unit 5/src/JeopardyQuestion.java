054970fa402912437845714f89b4bc1d2a353365
EDUARDO RACY <RACYEDU000@auburn.wednet.edu>
2022-04-08 13:45:08 -0700

    Jeopardy

---
+++ Unit 5/Unit 5/src/JeopardyQuestion.java
@@ -0,0 +1,20 @@
+public class JeopardyQuestion {
+    Scanner input = new Scanner(System.in);
+    private String question;
+    private String answer;
+    private String category;
+    private int difficultyLevel;
+    
+    public JeopardyQuestion(String question, String answer, String category, in difficultyLevel) {
+        this.question = quetsion;
+        this.answer = answer;
+        this.category = category;
+        this.difficultyLevel = difficultyLevel;
+    }
+
+    public void askQuestion(Scanner input) {
+        System.out.println(question);
+        System.out.println("");
+    }
+
+}
