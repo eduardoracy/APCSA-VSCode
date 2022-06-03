6afa3ccec031df847a2fdadc602abd5786d5f9cd
EDUARDO RACY <RACYEDU000@auburn.wednet.edu>
2022-06-03 13:47:21 -0700

    handman brain process

---
+++ Unit 5/Unit 5/src/TestingIO.java
@@ -0,0 +1,23 @@
+
+public class TestingIO {
+
+    public static void main(String[] args) {
+
+        textId writer = new textId("test.txt");
+
+        writer.writeLn("I dont know", false);
+
+        System.out.println(writer.readLn(1));
+
+        writer.writeLn("line 2", true);
+
+        System.out.println(writer.readLn(2));
+
+        writer.writeLn("8", false);
+
+        if (writer.readInt(1) == 8)
+            System.out.println("Line 1 is 8");
+
+    }
+
+}
