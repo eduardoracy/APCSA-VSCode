7a867ed09baeae0bc794c6629a14306a36bf448d
EDUARDO RACY <RACYEDU000@auburn.wednet.edu>
2022-06-07 13:46:04 -0700

    logic theoretically working

--- Unit 5/Unit 5/src/Hangman.java
+++ Unit 5/Unit 5/src/Hangman.java
@@ -2,53 +2,93 @@ import java.util.Scanner;
 
 public class Hangman {
     public static void main(String[] args) {
-        StringBuilder sb = new StringBuilder();
-        System.out.println(sb.underline("HELLO THERE"));
+        new Hangman().startGame();
     }
+
     Scanner console = new Scanner(System.in);
+
     public void startGame() {
-        console.useDelimiter("");
         boolean continuePlay = true;
         String[] arr = new String[0];
         while (continuePlay) {
             Utility.startScreen("Hangman", arr, console);
-            logic();
+            continuePlay = endScreen(logic());
         }
+        Utility.textCenter("Thank you for playing", ' ');
     }
 
-    public void logic() {
+    public int logic() {
         Utility.clearScreen();
-        
+
         String secretPhrase = phraseInputSelection();
         char[][] secretArr = new char[2][secretPhrase.length()];
         secretArr[0] = secretPhrase.toCharArray();
-        int turnCounter = 1;
+        int turnCounter = 1, incorrectGuesses = 0;
 
-        boolean gameOver = true;
-        while (gameOver) {
-            gameOver = playerTurn(secretArr, turnCounter);
+        while (true) {
+            incorrectGuesses = playerTurn(secretArr, turnCounter++, incorrectGuesses);
+            if (secretArr[0].equals(secretArr[1]) || turnCounter > 5) {
+                break;
+            }
         }
+        return turnCounter;
     }
 
-    public boolean playerTurn(char[][] secret, int turnCounter) {
+    public int playerTurn(char[][] secret, int turnCounter, int incorrectGuesses) {
         Utility.clearScreen();
-        Utility.printTurnHeader(String.format("Turn: &s", turnCounter), "Player");
+        Utility.printTurnHeader(String.format("Turn: %s", turnCounter), "Player");
 
-        //printBoard();
-        //printSecretWordGuesses;
+        printBoard(secret, incorrectGuesses);
 
         System.out.println("Enter a guess for a letter: ");
-        boardMarker(console.next());
-        return false;
+
+        return boardMarker(secret, console.next().charAt(0)) ? incorrectGuesses : incorrectGuesses++;
     }
 
-    public void boardMarker(String guess) {
-        
+    public boolean boardMarker(char[][] secret, char guess) {
+        boolean marked = false;
+        for (int i = 0; i < secret.length; i++) {
+            if (secret[0][i] == guess) {
+                secret[1][i] = guess;
+                marked = true;
+            }
+        }
+        return marked;
+    }
+
+    public void printBoard(char[][] secret, int incorrectGuesses) {
+        System.out.printf("Incorrect Guesses: %s\n", incorrectGuesses);
+
+        printSecretWordGuesses(secret);
+    }
+
+    public void printSecretWordGuesses(char[][] secret) {
+        StringBuilder sb = new StringBuilder();
+        for (char letter : secret[1]) {
+            System.out.print(sb.underline(String.format("%s", letter)) + " ");
+        }
+        System.out.println();
     }
 
     public String phraseInputSelection() {
         Utility.printTurnHeader("Secret Phrase Selection", "Game Master");
         Utility.textCenter("Game Master please input secret phrase for player to guess", ' ');
+        console.nextLine();
         return console.nextLine().trim();
+    }
+
+    public boolean endScreen(int turnCounter) {
+        Utility.printTurnHeader("Hangman", "Game Over");
+        System.out.println();
+
+        String status = turnCounter > 5 ? "You lost..." : "Congratulations, you've won";
+        Utility.textCenter(status, ' ');
+
+        Utility.textCenter("Would you like to return to start screen?", ' ');
+        if (console.next().equalsIgnoreCase("no")) {
+            return false;
+        } else {
+            return true;
+        }
     }
 }
